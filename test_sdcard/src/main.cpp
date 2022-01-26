7f9e9ff398a1a858c94dae18c287db5828c95ead
Mehdi Bagherimofidi <mehdi.bagherimofidi@yahoo.com>
2022-01-26 17:53:13 +0330

    add comment

--- test_sdcard/src/main.cpp
+++ test_sdcard/src/main.cpp
@@ -8,6 +8,7 @@
 #define MISO  12
 #define MOSI  13
 #define CS  27
+//comment
 
 void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
   Serial.printf("Listing directory: %s\n", dirname);
